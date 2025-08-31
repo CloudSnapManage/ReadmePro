8598c7d0224d6d4be8fe49ba6f8db52fac7276de
Shrijan Paudel <std.shrijan@gmail.com>
2025-08-31 15:09:29 +0000

    18s Run npx --no-install next build ⚠ No build cache found. Please confi

--- src/components/ui/resizable.tsx
+++ src/components/ui/resizable.tsx
@@ -1,12 +1,12 @@
 "use client"
 
 import * as React from "react"
-import { ImperativePanelHandle, Panel, PanelGroup } from "react-resizable-panels"
+import { Panel, PanelGroup, type ImperativePanelGroupHandle } from "react-resizable-panels"
 
 import { cn } from "@/lib/utils"
 
 const ResizablePanelGroup = React.forwardRef<
-  ImperativePanelHandle,
+  ImperativePanelGroupHandle,
   React.ComponentProps<typeof PanelGroup>
 >(({ className, ...props }, ref) => (
   <PanelGroup
@@ -28,6 +28,7 @@ const ResizableHandle = React.forwardRef<
 >(({ className, withHandle, ...props }, ref) => (
   <div
     ref={ref}
+    data-panel-resize-handle
     className={cn(
       "relative flex w-px items-center justify-center bg-border after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:-translate-y-1/2 data-[panel-group-direction=vertical]:after:translate-x-0 [&[data-panel-group-direction=vertical]>div]:rotate-90",
       className
